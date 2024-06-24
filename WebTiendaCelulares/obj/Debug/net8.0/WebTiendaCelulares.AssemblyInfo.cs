e7817f803c75b14e1aceb5968b9482c880624aa7
Luis Miguel Cruz Gareca <lc109070@gmail.com>
2024-06-24 08:32:56 -0400

    arreglos

--- WebTiendaCelulares/obj/Debug/net8.0/WebTiendaCelulares.AssemblyInfo.cs
+++ WebTiendaCelulares/obj/Debug/net8.0/WebTiendaCelulares.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTiendaCelulares")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79458c8ed9b7a6789d2b639bdd333d40e7f7059f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06dafcaa38ff06b271efc6d6a99efb8e2255319")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTiendaCelulares")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTiendaCelulares")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
